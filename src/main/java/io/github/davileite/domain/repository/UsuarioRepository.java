2515f073b4e48df9c2b604b2b2e365c9a3e103f8
Davi <deussecreadger@gmail.com>
2024-03-05 11:35:13 -0300

    feat: finalizadno configuração do JWT e implementando metodo de autenticação de usuarios

---
+++ src/main/java/io/github/davileite/domain/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package io.github.davileite.domain.repository;
+
+import io.github.davileite.domain.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByLogin(String login);
+}
